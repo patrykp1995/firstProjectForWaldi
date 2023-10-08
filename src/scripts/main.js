c73f4c827e10b1b4f73b8e16f70a01eea8563546
Waldekk95 <86876255+Waldekk95@users.noreply.github.com>
2023-10-08 09:28:56 +0200

    prettier
    prettier

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,45 +1,50 @@
-let searchInput = document.querySelector('input');
-let placesBoxes = document.getElementsByClassName('places__box');
-let test = document.querySelector('h2')
+let searchInput = document.querySelector("input");
+let placesBoxes = document.getElementsByClassName("places__box");
+let test = document.querySelector("h2");
 
 for (const box of placesBoxes) {
-  box.addEventListener('click', () => {
-    const currentCity = box.id
+  box.addEventListener("click", () => {
+    const currentCity = box.id;
     console.log(currentCity);
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&appid=c0734420e480e99c74ca1919537e7afe`)
-    .then(response => response.json())
-    .then(data => {
-      console.log('This is API response',data)
-      let country = data.city.country
-      let name = data.city.name
-      let population = data.city.population
-      let pressure = data.list[0].main.pressure
-      let weather = data.list[0].weather[0].main
-      test.innerHTML = `Kraj: ${country}`
-      test.insertAdjacentHTML("afterbegin", `<h2>Nazwa miasta: ${name}</h2>`)
-      test.insertAdjacentHTML("afterbegin", `<h2>Populacja: ${population} mieszkańców</h2>`)
-      test.insertAdjacentHTML("afterbegin", `<h2>hPa: ${pressure}</h2>`)
-      test.insertAdjacentHTML("afterbegin", `<h2>ikonka: ${weather}</h2>`)
-    })
-    .catch(error => console.error(error))
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&appid=c0734420e480e99c74ca1919537e7afe`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("This is API response", data);
+        let country = data.city.country;
+        let name = data.city.name;
+        let population = data.city.population;
+        let pressure = data.list[0].main.pressure;
+        let weather = data.list[0].weather[0].main;
+        test.innerHTML = `Kraj: ${country}`;
+        test.insertAdjacentHTML("afterbegin", `<h2>Nazwa miasta: ${name}</h2>`);
+        test.insertAdjacentHTML(
+          "afterbegin",
+          `<h2>Populacja: ${population} mieszkańców</h2>`
+        );
+        test.insertAdjacentHTML("afterbegin", `<h2>hPa: ${pressure}</h2>`);
+        test.insertAdjacentHTML("afterbegin", `<h2>ikonka: ${weather}</h2>`);
+      })
+      .catch((error) => console.error(error));
   });
 }
 
 const searchCity = () => {
-    if (searchInput.value !== '') {
-    console.log(searchInput.value)
-    } else {
-      alert('Wpisz nazwę miasta!')
-    }
+  if (searchInput.value !== "") {
+    console.log(searchInput.value);
+  } else {
+    alert("Wpisz nazwę miasta!");
   }
+};
 
 const enterKeyCheck = (e) => {
-    if(e.key === 'Enter') {
-      searchCity()
-    }
+  if (e.key === "Enter") {
+    searchCity();
   }
+};
 
-searchInput.addEventListener('keyup', enterKeyCheck)
+searchInput.addEventListener("keyup", enterKeyCheck);
 
 // https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=c0734420e480e99c74ca1919537e7afe
 
